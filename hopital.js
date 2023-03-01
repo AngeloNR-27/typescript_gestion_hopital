46ef0883258e3b16a4c1840ac0debd030b5ed2b8
AngeloNR-27 <rabetsimbaangelo@gmail.com>
2023-03-01 08:17:53 +0300

    modification et amelioration du code et ajout de la variable tarifsChambres

--- hopital.js
+++ hopital.js
@@ -1,16 +1,15 @@
-var Date_Arriver = new Date(2023, 0, 31);
-var Sejour = 5;
-var Date_Depart = new Date(2023, 1, 5);
-var ClasseChambre = {
+var dateArrivee = new Date(2023, 0, 31);
+var dureeSejour = 5;
+var dateDepart = new Date(2023, 1, 5);
+var tarifsChambres = {
     1: 350,
     2: 200,
     3: 150
 };
-var Montant = 0;
-console.log("Arrival date: ", Date_Arriver.toLocaleDateString());
-console.log("Stay duration: ", Sejour, "nights");
-console.log("Departure date: ", Date_Depart.toLocaleDateString());
-for (var Classe in ClasseChambre) {
-    Montant = ClasseChambre[Classe] * Sejour;
-    console.log("Total \u00E0 payer pour la chambre de classe ".concat(Classe, ": $"), Montant);
+for (var classeChambre in tarifsChambres) {
+    var montantTotalChambre = tarifsChambres[classeChambre] * dureeSejour;
+    console.log("Total \u00E0 payer pour la chambre de classe ".concat(classeChambre, ": $").concat(montantTotalChambre));
 }
+console.log("date arriver: ", dateArrivee.toLocaleDateString());
+console.log("duree de sejour: ", dureeSejour, "nights");
+console.log("date de depart: ", dateDepart.toLocaleDateString());
--- hopital.ts
+++ hopital.ts
@@ -1,18 +1,17 @@
-const Date_Arriver = new Date(2023, 0, 31);
-const Sejour = 5;
-const Date_Depart = new Date(2023, 1, 5);
-const ClasseChambre = {
+const dateArrivee = new Date(2023, 0, 31);
+const dureeSejour = 5;
+const dateDepart = new Date(2023, 1, 5);
+const tarifsChambres = {
   1: 350,
   2: 200,
   3: 150
 };
 
-let Montant = 0;
-
-console.log("Arrival date: ", Date_Arriver.toLocaleDateString());
-console.log("Stay duration: ", Sejour, "nights");
-console.log("Departure date: ", Date_Depart.toLocaleDateString());
-for (var Classe in ClasseChambre) {
-  Montant = ClasseChambre[Classe] * Sejour;
-  console.log("Total \u00E0 payer pour la chambre de classe ".concat(Classe, ": $"), Montant);
+for (const classeChambre in tarifsChambres) {
+  const montantTotalChambre = tarifsChambres[classeChambre] * dureeSejour;
+  console.log(`Total à payer pour la chambre de classe ${classeChambre}: $${montantTotalChambre}`);
 }
+
+console.log("date arriver: ", dateArrivee.toLocaleDateString());
+console.log("duree de sejour: ", dureeSejour, "nights");
+console.log("date de depart: ", dateDepart.toLocaleDateString());
